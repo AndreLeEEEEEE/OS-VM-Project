bc62985af5dc00dc2178ba04d2cea6422231a689
Sai Anurag <60319638+CloudCrewAtWork@users.noreply.github.com>
2023-04-01 17:10:38 -0600

    Add files via upload

---
+++ Benchmarks/cputests.py
@@ -0,0 +1,29 @@
+import psutil
+import time
+
+data = []
+
+while True:
+    # Ask user for memory size in GB
+    size = float(input("Enter memory size in GB: "))
+
+    try:
+        # Allocate memory
+        data.append(bytearray(size * 1024 * 1024 * 1024))
+    except MemoryError:
+        print("Memory overloaded. Program crashed.")
+        break
+
+    # Get CPU usage percentage
+    cpu_percent = psutil.cpu_percent()
+
+    # Get memory usage
+    mem = psutil.virtual_memory()
+    mem_used_percent = mem.percent
+
+    # Print CPU and memory usage
+    print("CPU usage:", cpu_percent, "%")
+    print("Memory usage:", mem_used_percent, "%")
+
+    # Wait for 1 second
+    time.sleep(1)
