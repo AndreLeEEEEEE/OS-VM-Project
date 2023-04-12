2304a840307863a7ad71db5195ff446403c65795
Kyu Ro <86690539+kyuro7793@users.noreply.github.com>
2023-04-12 12:58:01 -0600

    Add files via upload

--- Benchmarks/networkbandwidth.py
+++ Benchmarks/networkbandwidth.py
@@ -3,36 +3,41 @@ import time
 import os
 import subprocess
 
-#gets ip address of VM
-def get_vm_ip():
-    ip_command = "ip route get 1 | awk '{print $NF;exit}'"
-    result = subprocess.check_output(ip_command, shell=True).decode('utf-8').strip()
-    return result
-
 #benchmark test for network bandwidth 
 #send data packet, receive data packet, measure time to send and receive, calculate bandwidth of time and data size 
 
 #set ip address and port
-host = get_vm_ip()
+host = '10.0.2.15'
 port = 80
-
+s
 #set data size
 data_size = 1024 * 1024 * 10  
 
 #create socket object and connect to host
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+except socket.error as err:
+    print(f"{err}")
+    exit()
 
-#time to send/receive data  
-start_time = time.time()
-s.sendall(b'0' * data_size)
-s.recv(data_size)
-end_time = time.time()
+loop = 50
+bw = []
 
-#calculate bandwidth in mbps
-active = end_time - start_time
-bandwidth = data_size * 8 / (active * 1000000)
-print(f"{bandwidth:.1f} mbps")
+for i in range(loop):
+    #time to send/receive data  
+    start_time = time.time()
+    s.sendall(b'0' * data_size)
+    s.recv(data_size)
+    end_time = time.time()
+
+    #calculate bandwidth in mbps
+    active = end_time - start_time
+    bandwidth = data_size * 8 / (active * 1000000)
+    bw.append(bandwidth)
+
+avg_bw = sum(bw) / len(bw)
+print(f"{avg_bw:.1f} mbps")
 
 #close socket 
 s.close()
